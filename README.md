110e5baebca98945a24de60a2d3c7ef7329c429b
Kevin Hester <kevinh@geeksville.com>
2019-03-06 05:00:24 -0800

    Initial checkin

---
+++ README.md
@@ -0,0 +1,15 @@
+# EZDevice-python
+
+This project is in alpha test - you probably don't want this yet.
+
+# Author
+
+Kevin Hester, kevinh@geeksville.com
+
+# License
+
+FIXME add MIT license
+
+# Contributing
+
+The root repository for this project is located at https://github.com/geeksville/ezdevice-python. Issues and pull requests are gratefully accepted.
---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ezdevice",
+    version="0.0.1",
+    description="Python tool & library for using ESP32 based Ezdevice.net projects",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geeksville/ezdevice-python",
+    author="Kevin Hester",
+    author_email="kevinh@geeksville.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=["esptool"],
+    entry_points={
+        "console_scripts": [
+            "ezdevice=ezdevice.__main__:main",
+        ]
+    },
+)
